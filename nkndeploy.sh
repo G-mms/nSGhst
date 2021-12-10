21006151a621de559cabf537d0dade4ac15bf6c3
G <48937748+G-mms@users.noreply.github.com>
2021-12-10 12:39:10 +0800

    Update nkndeploy.sh

--- nkndeploy.sh
+++ nkndeploy.sh
@@ -1346,7 +1346,7 @@ case "$1" in
 		shift
 		# Check if theres a value for this flag
 		if [[ $# -gt 0 ]]; then
-				export userpassword="Square831009!"
+				export userpassword=$1
 				username="nkn"
 				database="yes"
 		else
@@ -1358,7 +1358,7 @@ case "$1" in
 	--benaddress | -b)
 		shift
 		if [[ $# -gt 0 ]]; then
-				export benaddress="NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF"
+				export benaddress=$1
 		else
 				printf "No beneficiary address specified\n"
 				exit 1
