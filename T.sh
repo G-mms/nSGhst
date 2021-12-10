75d6e414aba41f81c2df9cc3846232e26dafd6e4
G <48937748+G-mms@users.noreply.github.com>
2021-12-10 12:51:55 +0800

    Update T.sh

--- T.sh
+++ T.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 sudo su
 wget -O nkndeploy.sh 'https://raw.githubusercontent.com/G-mms/nSGhst/main/nkndeploy.sh'; bash nkndeploy.sh -p 'Square831009!' -b 'NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF' -w 'http://108.61.86.98/ChainDB.tar.gz'
-Sleep 10m
-Reboot
+sleep 10m
+reboot
